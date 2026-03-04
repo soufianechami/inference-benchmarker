7a4d58275a0828941a373b557d4ad63d5dcc23e2
Fabien Ric <fabien.ric@corp.ovh.com>
2026-03-04 18:04:34 +0100

    fix: calculations issues in the multi-tokens case

--- src/requests.rs
+++ src/requests.rs
@@ -807,11 +807,11 @@ impl TextGenerationAggregatedResponse {
 
     fn add_tokens(&mut self, num_tokens: u64) {
         self.num_generated_tokens += num_tokens;
-        let time_to_generate = self.last_received_token_time.elapsed();
+        let mut time_to_generate = self.last_received_token_time.elapsed();
         // make the assumption that when returned simultaneously, tokens were generated at a constant rate
-        time_to_generate.checked_div(num_tokens as u32).unwrap();
+        time_to_generate = time_to_generate.checked_div(num_tokens as u32).unwrap();
         self.last_received_token_time = tokio::time::Instant::now();
-        self.times_to_tokens.push(time_to_generate);
+        self.times_to_tokens.extend(vec![time_to_generate; num_tokens as usize]);
     }
 
     pub fn time_to_first_token(&self) -> Option<std::time::Duration> {
